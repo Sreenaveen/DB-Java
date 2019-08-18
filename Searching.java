bde1d797fe41d3f2b2dc3c38dee68b5f848662eb
Sree Naveen <46847700+Sreenaveen@users.noreply.github.com>
2019-08-18 12:31:57 +1000

    initial commit

---
+++ Searching.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author sreenaveen
+ *
+ */
+public class Searching {
+
+	
+	/**
+	 * @param arr
+	 * @param numberToSearch
+	 * @return
+	 */
+	public boolean collectionSetSearch(int arr[], Integer numberToSearch) {
+		Set<Integer> intSet = new HashSet<Integer>();
+		for (int i : arr) {
+			intSet.add(i);
+		}
+		return intSet.contains(numberToSearch);
+	}
+
+	
+	/**
+	 * @param arr
+	 * @param numberToSearch
+	 * @return
+	 */
+	public boolean arraysSearch(int arr[], int numberToSearch) {
+
+		int retVal = Arrays.binarySearch(arr, numberToSearch);
+		if (retVal != -1)
+			return true;
+		else
+			return false;
+	}
+
+	/**
+	 * @param arr
+	 * @param item
+	 * @return
+	 */
+	public  boolean normalSearch(int[] arr, int item) {
+	      for (int n : arr) {
+	         if (item == n) {
+	            return true;
+	         }
+	      }
+	      return false;
+	   }
+
+	/**
+	 * @param args
+	 * @throws IOException
+	 */
+	public static void main(String args[]) throws IOException {
+		Searching obj = new Searching();
+		int[] list_of_numbers = { 9, 55, 10, 12, 23, 244, 2131 };
+
+		obj.collectionSetSearch(list_of_numbers, 23);
+
+		obj.arraysSearch(list_of_numbers, 45);
+		obj.normalSearch(list_of_numbers, 45);
+
+	}
+}
